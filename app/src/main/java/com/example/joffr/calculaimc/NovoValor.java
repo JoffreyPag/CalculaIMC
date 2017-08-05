f7b77504bca48417d60b5c4a63a4919011f4358d
Heriberto Filho <heriberto.filho@outlook.com>
2017-08-05 14:53:18 -0300

    Atualizações, comentei a maioria.

--- app/src/main/java/com/example/joffr/calculaimc/NovoValor.java
+++ app/src/main/java/com/example/joffr/calculaimc/NovoValor.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.view.ViewPropertyAnimatorCompatSet;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -12,34 +13,37 @@ public class NovoValor extends AppCompatActivity {
 
     TextView tv;
     boolean tipe;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_novo_valor);
-        tv = (TextView)findViewById(R.id.opcao);
+        tv = (TextView) findViewById(R.id.opcao);
         Bundle b = new Bundle();
         tipe = b.getBoolean("tipo");
-        if(tipe){
-            tv.setText("Peso: ");
-        }else{
-            tv.setText("Altura: ");
+        if (tipe) {
+            tv.setText(R.string.peso_titulo);
+        } else {
+            tv.setText(R.string.altura_titulo);
         }
 
     }
 
 
-    public void AlteraValor(View v){
-        EditText et = (EditText)findViewById(R.id.texto);
+    public void AlteraValor(View v) {
+        EditText et = (EditText) findViewById(R.id.texto);
         String novo = et.getText().toString();
-        Bundle b = new Bundle();
-        b.putString("novoValor", novo);
+        //Não precisa de um bundle nesse caso.
+        //Bundle b = new Bundle();
+        //b.putString("novoValor", novo);
         Intent i = new Intent();
-        i.putExtras(b);
+        i.putExtra("novoValor", novo);
+        //i.putExtras(b);
         setResult(RESULT_OK, i);
         finish();
     }
 
-    public void Cancelamento(View v){
+    public void Cancelamento(View v) {
         setResult(RESULT_CANCELED);
         finish();
     }
