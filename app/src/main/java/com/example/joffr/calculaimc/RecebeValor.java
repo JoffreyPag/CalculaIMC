cc4c9192d191752cd38873ee54f46274909fe0fa
JoffreyPag <joffreypag@gmail.com>
2017-08-04 21:44:52 -0300

    Initial commit

---
+++ app/src/main/java/com/example/joffr/calculaimc/RecebeValor.java
@@ -0,0 +1,13 @@
+package com.example.joffr.calculaimc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecebeValor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recebe_valor);
+    }
+}
