a1813287f33847c0d42401b72b5eda23fa52a15c
joffr <joffreypag@gmail.com>
2017-08-06 15:41:51 -0300

    att

--- app/src/main/java/com/example/joffr/calculaimc/MainActivity.java
+++ app/src/main/java/com/example/joffr/calculaimc/MainActivity.java
@@ -45,24 +45,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (resultCode == RESULT_OK) {
             if (requestCode == NOVOPESO) {
-                Toast.makeText(this, "Retornou", Toast.LENGTH_SHORT).show();
+                //Toast.makeText(this, "Retornou", Toast.LENGTH_SHORT).show();
                 /* Você tava criando um bundle e não recebendo, veja que na assinatura da função você recebe
                 uma variável chamada data e ela é do tipo intent, portanto, esta variável é a intent retornada pela activity.
 
                 Aqui estou pegando o valor direto da Intent e não de um bundle, pois não achei necessário.
                 Se tivesse um bundle: i.getBundle.getStringExtra("novoValor");*/
                 Intent i = data;
-                //String novo = i.getStringExtra("novoValor");
-                //double nonovo = Double.parseDouble(novo);
-                double novo = Double.parseDouble(i.getStringExtra("novoValor"));
+                peso = Double.parseDouble(i.getStringExtra("novoValor"));
                 //É melhor você fazer dessa forma a baixo, no mínimo é mais bonito.
-                tp.setText(String.valueOf(novo));
+                tp.setText(String.valueOf(peso));
             } else if (requestCode == NOVAALTURA) {
-                Toast.makeText(this, "ok né?", Toast.LENGTH_SHORT).show();
+                //Toast.makeText(this, "ok né?", Toast.LENGTH_SHORT).show();
                 Intent i = data;
-                String novo = i.getStringExtra("novoValor");
+                //String novo = i.getStringExtra("novoValor");
+                altura = Double.parseDouble(i.getStringExtra("novoValor"));
                 //Mesma coisa do setText acima.
-                ta.setText(String.valueOf(novo));
+                ta.setText(String.valueOf(altura));
             }
         } else {
             Toast.makeText(this, "cancelado", Toast.LENGTH_SHORT).show();
